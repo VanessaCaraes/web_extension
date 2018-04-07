2789ce83bf2a0853397563f44ef4dd77f3f26b70
Vanessa <caraes06@hotmail.fr>
2018-04-07 17:32:50 +0200

    test

---
+++ background.js
@@ -0,0 +1,33 @@
+/*document.body.style.border = "5px solid red";*/
+
+browser.theme.update ({
+  "images": {
+    "headerURL": "",
+    
+  },
+  
+"properties":{
+"additionnal_backgrounds_alignement":["top"],
+"additionnal_backgrounds_tiling":["repeat"]
+  },
+
+  "colors": {
+    /*"accentcolor": "#CF723F",
+    "textcolor": "white",*/
+
+    "accentcolor": "#CF723F",
+       "textcolor": "white",
+       "toolbar": "#cf653f",
+       "toolbar_text": "pink",
+       "toolbar_field": "#FAF0E6",
+       "toolbar_field_border": "white",
+       "toolbar_field_text": "#be3fcf",
+       "toolbar_top_separator": "red",
+       "toolbar_bottom_separator": "white",
+       "toolbar_vertical_separator": "white"
+
+  },
+
+
+});
+
